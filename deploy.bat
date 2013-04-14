c0298ab0e6ae3b36cfaa34546d6e15cf97a11697
Mark Fowler <growler1970@gmail.com>
2013-04-14 15:37:21 +0100

    Added notes on lessons learned and fixed deploy script issue

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,6 @@ net stop "Apache Tomcat 7.0 Tomcat7"
 
 del %CATALINA_HOME%\webapps\AudioServerWeb.war
 rmdir /S /q %CATALINA_HOME%\webapps\AudioServerWeb
-copy target\AudioServerWeb.war %CATALINA_HOME%\webapps\
+copy target\AudioServerWeb.war "%CATALINA_HOME%\webapps\"
 
 net start "Apache Tomcat 7.0 Tomcat7"

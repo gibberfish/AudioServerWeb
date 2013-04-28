914f2ad8dd06a8702e6cd025891ab4d1af1a60b5
Mark Fowler <growler1970@gmail.com>
2013-04-28 10:18:21 +0100

    Ensure the deploy deletes the correct folder before deploying

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,8 @@
 
 net stop "Apache Tomcat 7.0 Tomcat7"
 
-del %CATALINA_HOME%\webapps\AudioServerWeb.war
-rmdir /S /q %CATALINA_HOME%\webapps\AudioServerWeb
+del "%CATALINA_HOME%\webapps\AudioServerWeb.war"
+rmdir /S /q "%CATALINA_HOME%\webapps\AudioServerWeb"
 copy target\AudioServerWeb.war "%CATALINA_HOME%\webapps\"
 
 net start "Apache Tomcat 7.0 Tomcat7"
